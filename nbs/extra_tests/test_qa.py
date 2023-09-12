5ce2a2c442062a3a7753e20b7a78aae068af0616
Arun S. Maiya <arun@maiya.net>
2023-09-12 15:53:41 -0400

    added convenience script for RAG

---
+++ nbs/extra_tests/test_qa.py
@@ -0,0 +1,45 @@
+"""
+This is a simple local test of basic RAG functionality.
+The "Exception ignored in: <function Llama.__del__ at 0x7fe2d96d7430>" from llama-cpp-python is
+ignored and doesn't affect functionality.
+"""
+
+from onprem import LLM, utils as U
+import os, tempfile, shutil
+
+
+
+if __name__ == "__main__":
+    # make a temporary directory for our vector database
+    vectordb_path = tempfile.mkdtemp()
+
+    # setup
+    llm = LLM(vectordb_path=vectordb_path)
+
+    # prompt
+    #saved_output = llm.prompt('Generate one cute name for a cat.')
+    print()
+
+    # download data
+
+    source_folder = tempfile.mkdtemp()
+    U.download('https://raw.githubusercontent.com/amaiya/onprem/master/nbs/sample_data/1/ktrain_paper.pdf', 
+              os.path.join(source_folder, 'ktrain.pdf'))
+    llm.ingest(source_folder)
+    assert(os.path.exists(source_folder))
+    print()
+    answer, sources = llm.ask("What is ktrain?")
+    assert(len(answer) > 8)
+    assert(len(sources)==4)
+    print()
+    U.download('https://raw.githubusercontent.com/amaiya/onprem/master/nbs/sample_data/3/state_of_the_union.txt', 
+              os.path.join(source_folder, 'sotu.txt'))
+    llm.ingest(source_folder)
+    print()
+    answers, sources = llm.ask("Who is Ketanji?")
+    assert(len(answer) > 8)
+    assert(len(sources)==4)
+    print()
+    shutil.rmtree(source_folder)
+    shutil.rmtree(vectordb_path)
+
