75973409dac0781934ecb97ca81c1fa16854c211
Arun S. Maiya <arun@maiya.net>
2023-09-26 10:03:27 -0400

    bump

--- onprem/__init__.py
+++ onprem/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.24"
+__version__ = "0.0.25"
 
 # reference: https://stackoverflow.com/questions/74918614/error-importing-seaborn-module-attributeerror/76760670#76760670
 import numpy as np
