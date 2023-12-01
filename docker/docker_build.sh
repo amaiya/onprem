3d49d6699a3a45c73a4a2c0a59bdf75ea58e5d13
Dale Visser <dvisser@ida.org>
2023-12-01 13:21:49 -0500

    First draft of CUDA version auto-detection

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]; then
+function print_guide() {
+    echo "Usage: $0"
+    echo
+    echo "NVIDIA CUDA support version will be automatically detected. Only"
+    echo "versions 11 and 12 are supported."
+    exit 1
+}
+
+# Only allow script to run as root
+if [ "$EUID" -ne 0 ]
+then
     echo "This script must be run with sudo."
     exit 1
 fi
 
-function print_guide() {
-    echo "Usage: $0 [CUDA version]"
-    echo "  where CUDA version may be 11 or 12. 12 is used if omitted"
-    exit 1
-}
-
-# Are we building CUDA 12 or CUDA 11?
-cuda_version=12
-if [ "$#" -eq 0 ]; then
-    echo "CUDA version not provided. Using default of ${cuda_version}"
-elif [ "$#" -eq 1 ]; then
-    if [ "$1" -ne 11 ] && [ "$1" -ne 12 ]; then
-        print_guide
-    fi
-    cuda_version=$1
-else
-  print_guide
+# Is the necessary NVIDIA stuff installed? Technically, we could build the image
+# without, but it is assumed that the container image will be used locally.
+if ! command -v nvidia-smi >/dev/null 2>&1
+then
+  echo "The nvidia-smi executable is not present. Are you sure you have an"
+  echo "NVIDIA device with NVIDIA drivers installed?"
+  exit 1
 fi
 
+# Are we building CUDA 12 or CUDA 11?
+cuda_version=`nvidia-smi | grep -P -o "CUDA Version: \d+(\.\d+)*" | grep -P -o "\d+" | head -n 1`
+if [ "$cuda_version" -ne 11 ] && [ "$cuda_vesion" -ne 12 ]
+then
+    print_guide
+fi
 
+# Set the suffix of the Dockerfile to use
 suffix="12.1.1"
 if [ "$cuda_version" -eq 11 ]; then
     suffix="11.6.1"
